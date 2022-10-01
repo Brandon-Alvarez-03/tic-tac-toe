755bfe840d5e7af1410b32b22e85e9af0f5c2a87
Brandon Alvarez <balvarez325@gmail.com>
2022-10-01 16:43:44 -0400

    Basic game structure done: Add Functionality

---
+++ script.js
@@ -0,0 +1,21 @@
+//make queries for each important item
+
+let p1Turn = document.querySelector(".player-one");
+let p2Turn = document.querySelector(".player-one");
+let boxes = document.querySelectorAll(".tile");
+let resetBtn = document.querySelector("#reset");
+
+// create function for change red and one for change blue
+function makeBlue(box) {
+  // box.style.background-color = 'blue'
+}
+
+//create an event listener to toggle for red or blue on each grid
+
+boxes.addEventListener("click", (e) => {
+  //change color red change color blue
+});
+
+//when each of those switches toggle the color on player one or player two
+
+//make reset button clear the board and clear colors from the players names
