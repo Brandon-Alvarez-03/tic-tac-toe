cc41904cb10f1a378db6a5b70806d89f361b69b2
Brandon Alvarez <balvarez325@gmail.com>
2022-10-01 20:54:43 -0400

    Current Player Tracking Good

--- script.js
+++ script.js
@@ -2,10 +2,16 @@
 
 let p1Turn = document.querySelector(".player-one");
 let p2Turn = document.querySelector(".player-one");
+
 let boxes = document.querySelectorAll(".tile");
 let resetBtn = document.querySelector("#reset");
+let playerOptions = ["playerOne", "playerTwo"];
+let currentPlayer =
+  playerOptions[Math.floor(Math.random() * playerOptions.length)];
+console.log(currentPlayer);
+
 console.log(boxes);
-// create function for change red and one for change blue
+// create functions for change red and one for change blue
 function makeBlue(box) {
   box.target.style.backgroundColor = `lightBlue`;
 }
@@ -13,21 +19,35 @@ function makeBlue(box) {
 function makeRed(box) {
   box.target.style.backgroundColor = `red`;
 }
+
 //create an event listener to toggle for red or blue on each grid
+// create tracker for each player and when clicked switch player
 boxes.forEach((tile) => {
   tile.addEventListener("click", (e) => {
     //change color red change color blue
-    if (e.target.style.backgroundColor != `blue`) {
+    if (
+      // e.target.style.backgroundColor == `lightgray` &&
+      currentPlayer == "playerTwo"
+    ) {
       makeRed(e);
-    }
-    if (e.target.style.backgroundColor != `red`) {
+      currentPlayer = "playerOne";
+    } else if (
+      // e.target.style.backgroundColor == `lightgray` &&
+      currentPlayer == "playerOne"
+    ) {
       makeBlue(e);
-    } else {
-      e.target.style.backgroundColor = `lightGray`;
+      currentPlayer = "playerTwo";
     }
   });
 });
 
-//when each of those switches toggle the color on player one or player two
+// make player in control visible when it's their turn
+
+// Establish win conditions and display win alert
 
 //make reset button clear the board and clear colors from the players names
+resetBtn.addEventListener("click", () => {
+  boxes.forEach((tile) => {
+    tile.style.backgroundColor = `lightgray`;
+  });
+});
